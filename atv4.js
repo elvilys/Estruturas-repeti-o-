852b62ecb255c1a525c34a25fb90fd466fbb0415
elvilys bolivar <elvilysbolivar@gmail.com>
2023-08-01 23:43:47 -0300

    finalizando atv 4

---
+++ atv4.js
@@ -0,0 +1,20 @@
+const nomeCliente = prompt("Digite o nome do cliente:");
+const sexoCliente = prompt("Digite o sexo do cliente (M para masculino, F para feminino):").toUpperCase();
+const valorCompras = parseFloat(prompt("Digite o valor total das compras:"));
+
+
+if (valorCompras) {
+  let desconto = 0;
+
+  if (sexoCliente === 'M') {
+    desconto = valorCompras * 0.05; 
+  } else if (sexoCliente === 'F') {
+    desconto = valorCompras * 0.13; 
+  } else {
+    alert("Sexo inválido! Por favor, digite 'M' para masculino ou 'F' para feminino.");
+  }
+
+  const precoComDesconto = valorCompras - desconto;
+  document.write(`Cliente: ${nomeCliente} Valor com desconto: R$ ${precoComDesconto} <br>`);
+
+}
